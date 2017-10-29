ac51abd6a2852db17f253cdd7b191af2d5f359ab
Mathieu Benoit <mabenoit@microsoft.com>
2017-10-29 19:25:25 -0400

    Add UnitTest project.

---
+++ src/MainWebApplication/XUnitTestProject/SampleUnitTest.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class SampleUnitTest
+    {
+        [Fact]
+        public void TwoPlusThreeEqualsFive()
+        {
+            //Arrange
+            var expectedResult = 5;
+
+            //Act
+            var result = Add(2, 3);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        int Add(int x, int y)
+        {
+            return y + y;
+        }
+    }
+}
